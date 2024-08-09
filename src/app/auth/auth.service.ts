13fd55cfcb87031111f330406d366f66ed5ac1c1
Albert <albertkhamzin93@gmail.com>
2024-08-09 15:30:22 +0300

    add token and refreshToken to cookies for authorization

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
 import {tap} from 'rxjs'
 import {TokenResponse} from './tokenResponse'
+import {CookieService} from 'ngx-cookie-service'
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,15 @@ import {TokenResponse} from './tokenResponse'
 export class AuthService {
   http = inject(HttpClient)
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/'
+  cookieService = inject(CookieService)
 
   token: null | string = null
   refreshToken: null | string = null
 
   get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token')
+    }
     return !!this.token
   }
 
@@ -30,6 +35,10 @@ export class AuthService {
       tap(val => {
         this.token = val.access_token
         this.refreshToken = val.refresh_token
+
+
+        this.cookieService.set('token', this.token)
+        this.cookieService.set('refreshToken', this.refreshToken)
       })
     )
   }
