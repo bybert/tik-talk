3067f5cad4336c95bdff674302ba291791fd52ba
Albert <albertkhamzin93@gmail.com>
2024-08-13 19:48:37 +0300

    add home, chat, search items with hover, focus stiles and icons for each Sidebar items

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
+import {SvgIconComponent} from '../svg-icon/svg-icon.component'
+import {RouterLink} from '@angular/router'
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    SvgIconComponent,
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {label: 'Моя страница', icon: 'home', link: 'home'},
+    {label: 'Чаты', icon: 'chat', link: 'chat'},
+    {label: 'Поиск', icon: 'search', link: 'search'}
+  ]
 }
