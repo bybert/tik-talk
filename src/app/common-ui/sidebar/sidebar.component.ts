3f0c3a07ba8dcf8ad60b8929f2455328aeb8c130
Albert <albertkhamzin93@gmail.com>
2024-08-13 20:57:12 +0300

    add Subscribers element with link, arrow svg and stylize this element

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core'
 import {SvgIconComponent} from '../svg-icon/svg-icon.component'
 import {RouterLink} from '@angular/router'
+import {SubscriberCardComponent} from './subscriber-card/subscriber-card.component'
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     SvgIconComponent,
-    RouterLink
+    RouterLink,
+    SubscriberCardComponent
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
