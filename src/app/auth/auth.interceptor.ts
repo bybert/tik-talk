3c29d3cdc50e2231c9e52f7307dc8912354f0d27
Albert <albertkhamzin93@gmail.com>
2024-08-13 09:51:22 +0300

    add refreshToken to interceptor, logout and saveTokens

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,15 +1,41 @@
-import {HttpInterceptorFn} from '@angular/common/http'
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http'
 import {AuthService} from './auth.service'
 import {inject} from '@angular/core'
+import {catchError, switchMap, throwError} from 'rxjs'
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = inject(AuthService)
+  const authService = inject(AuthService)
+  const token = authService.token
 
   if (!token) return next(req)
-  req = req.clone({
+  return next(addToken(req, token)).pipe(
+    catchError(error => {
+      if (error.status === 403) {
+        return refreshAndProceed(authService, req, next)
+      }
+      return throwError(error)
+    })
+  )
+}
+
+const refreshAndProceed = (
+  authService: AuthService,
+  req: HttpRequest<any>,
+  next: HttpHandlerFn
+) => {
+
+  return authService.refreshAuthToken()
+    .pipe(
+      switchMap(res => {
+        return next(addToken(req, res.access_token))
+      })
+    )
+}
+
+const addToken = (req: HttpRequest<any>, token: string) => {
+  return req.clone({
     setHeaders: {
       Authorization: `Bearer ${token}`
     }
   })
-  return next(req)
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import {inject, Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
-import {tap} from 'rxjs'
+import {catchError, tap, throwError} from 'rxjs'
 import {TokenResponse} from './tokenResponse'
 import {CookieService} from 'ngx-cookie-service'
+import {Router} from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   http = inject(HttpClient)
+  router = inject(Router)
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/'
   cookieService = inject(CookieService)
 
@@ -32,14 +34,37 @@ export class AuthService {
       `${this.baseApiUrl}token`,
       fd,
     ).pipe(
-      tap(val => {
-        this.token = val.access_token
-        this.refreshToken = val.refresh_token
+      tap(val => this.saveTokens(val))
+    )
+  }
 
-
-        this.cookieService.set('token', this.token)
-        this.cookieService.set('refreshToken', this.refreshToken)
+  refreshAuthToken() {
+    return this.http.post<TokenResponse>(
+      `${this.baseApiUrl}token`,
+      {
+        refresh_token: this.refreshToken,
+      }
+    ).pipe(
+      tap(val => this.saveTokens(val)),
+      catchError(error => {
+        this.logout()
+        return throwError(error)
       })
     )
+  }
+
+  logout() {
+    this.cookieService.deleteAll()
+    this.token = null
+    this.refreshToken = null
+    this.router.navigate(['/login'])
+  }
+
+  saveTokens(res: TokenResponse) {
+    this.token = res.access_token
+    this.refreshToken = res.refresh_token
+
+    this.cookieService.set('token', this.token)
+    this.cookieService.set('refreshToken', this.refreshToken)
   }
 }
